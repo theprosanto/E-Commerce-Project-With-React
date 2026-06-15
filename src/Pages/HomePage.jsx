57372412b8891ce0d0174126ee26f607607537d1
theprosanto <theprosanto@gmail.ccom>
2026-06-15 15:17:40 +0600

    up

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -15,20 +15,33 @@ const HomePage = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
+        appendDots: dots => (
+      <div>
+        <ul style={{ margin: "30px" }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div
+        className='w-3 h-3 rounded-full bg-gray-400'
+      >
+    
+      </div>
+    )
     };
     return (
         <div className=''>
             <Container>
                 <div className='flex gap-13'>
                     <div className='w-[20%] border-r'>
-                        <ul className='space-y-4 mt-12'>
-                            <div className='flex gap-12.75 items-center'>
-                                <li>Woman’s Fashion </li>
+                        <ul className='space-y-4 mt-12 w-54.25'>
+                            <div className='flex gap-13.75 items-center'>
+                                <li>Woman’s Fashion</li>
                                 <MdArrowForwardIos />
                             </div>
 
 
-                            <div className='flex gap-12.75 items-center'>
+                            <div className='flex gap-20.25 items-center'>
                                 <li>Men’s Fashion</li>
                                 <MdArrowForwardIos />
                             </div>
@@ -43,6 +56,12 @@ const HomePage = () => {
                     </div>
                     <div className='w-[80%] mt-12'>
                         <Slider {...settings}>
+                            <div>
+                                <img className='w-full' src={BannarAds} alt="" />
+                            </div>
+                            <div>
+                                <img className='w-full' src={BannarAds} alt="" />
+                            </div>
                             <div>
                                 <img className='w-full' src={BannarAds} alt="" />
                             </div>
